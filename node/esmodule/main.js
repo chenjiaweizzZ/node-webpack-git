27d1cd861ad43749c4b64e30d8e70890def46c92
陈家伟 <19650@163.com>
2024-09-19 10:55:17 -0400

    esmodule

---
+++ node/esmodule/main.js
@@ -0,0 +1,5 @@
+// import { name as fname, age, sayHello } from "./utils.js"
+// console.log(fname, age, sayHello())
+
+import * as utils from "./utils.js"
+console.log(utils.name)
---
+++ node/esmodule/utils.js
@@ -0,0 +1,21 @@
+// const name = "陈家伟"
+// const age = 12
+
+// function getName() {
+//     return "myName"
+// }
+
+//导出方式1
+// export {
+//     name as utilsName,
+//     age,
+//     getName
+// }
+
+//导出方式2
+export const name = "陈家伟"
+export const age = 12
+export function sayHello() {
+    return "hello"
+}
+
